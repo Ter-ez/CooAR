363d7ebc34e04b2b3209d9f3a4d836f714e611d4
Terez <t.cahova@gmail.com>
2023-03-09 21:09:57 +0100

    keyboard

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
+const kBoard = document.querySelector(".simple-keyboard");
+const inpt = document.querySelector(".input");
+
 document.querySelector("button").addEventListener("click", ()=> {
-    window.alert('Hello')
+    if (kBoard.style.display == "none") {
+        kBoard.style.display = "block";
+        inpt.style.display = "block";
+    }
+    else {
+        kBoard.style.display = "none";
+        inpt.style.display = "none";
+    }
   })
+
 
   const Keyboard = window.SimpleKeyboard.default;
 
   const myKeyboard = new Keyboard({
     onChange: input => onChange(input),
-    onKeyPress: button => onKeyPress(button)
+    onKeyPress: button => onKeyPress(button),
+    layout: {
+        'default': [
+          '1 2 3 4 5 6 7 8 9 0',
+          'q w e r t y u i o',
+          'p a s d f g h j k',
+          'l z x c v b n m {bksp}',
+          '{space}'
+        ]
+      }
   });
   
   function onChange(input) {
